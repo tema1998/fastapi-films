1d4f1b919eff3b506ea94dc141c688af54074eda
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-03-16 10:19:41 +0300

    Create models Create DB queries

--- app/models.py
+++ app/models.py
@@ -1,10 +1,45 @@
+from datetime import datetime
+
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import Session
+from sqlalchemy.exc.declarative import declarative_base
 
 from config import DATABASE_URL
+
+Base = declarative_base()
 
 
 def connect_db():
     engine = create_engine(DATABASE_URL, connect_args={})
     session = Session(bind=engine.connect())
     return session
+
+
+class User:
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    nickname = Column(String)
+    created_at = Column(String, default=datetime.utcnow())
+
+
+class Film(Base):
+    __tablename__ = 'films'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    title = Column(String)
+    description = Column(String)
+    created_at = Column(String, default=datetime.utcnow())
+
+
+class AuthToken(Base):
+    __tablename__ = 'auth.token'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    token = Column(String)
+    created_at = Column(String, default=datetime.utcnow())
---
+++ scripts/create_db.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from app.config import DATABASE_URL
+
+
+def main():
+    engine = create_engine(DATABASE_URL)
+    session = Session(bind=engine.connect())
+
+    session.execute("""create table user(
+    id integer not null primary key,
+    email varchar(256),
+    password varchar(256),
+    first_name varchar(256),
+    last_name varchar(256),
+    created_at varchar(256),
+    );""")
+
+    session.execute("""create table auth_token(
+    id integer not null primary key,
+    user_id integer references users,
+    token varchar(256),
+    created_at varchar(256),
+    );""")
+
+    session.execute("""create table film(
+    id integer not null primary key,
+    user_id integer references users,
+    title varchar(256),
+    description varchar(256),
+    created_at varchar(256),
+    );""")
+
+    session.close()
+
+if __name__ == '__main__':
+    main()
