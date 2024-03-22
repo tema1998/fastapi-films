0558343c6a43595ca628a7528749fa6e4504496e
tema1998 <104845356+tema1998@users.noreply.github.com>
2024-03-22 14:02:25 +0300

    Add getting current user by JWT Refactor handlers to /core/ and /auth/

--- api/handlers.py
+++ api/handlers.py
@@ -1,101 +1,13 @@
-import json
-import uuid
-from datetime import timedelta
-from typing import Union
-
 from fastapi import APIRouter, Depends
 from fastapi.encoders import jsonable_encoder
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from jose import jwt
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 from starlette.exceptions import HTTPException
 
-import settings
-from api.models import UserCreate, ShowFilm, ShowSeries, ShowFilmSchema, ShowSeriesSchema, Token
-from api.utils import Hasher, create_access_token
-from db.dals import UserDAL, FilmDAL, SeriesDAL
-from db.models import User, AuthToken
+from api.models import ShowFilmSchema, ShowSeriesSchema
+from db.dals import FilmDAL, SeriesDAL
 from db.session import connect_db
-from api.auth import check_auth_token
 
-router = APIRouter()
-
-
-async def _get_user_by_email_for_auth(email:str, db: AsyncSession):
-    async with db as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            return await user_dal.get_user_by_email(
-                email=email,
-            )
-
-
-async def authenticate_user(email: str, password: str, db: AsyncSession) -> Union[User, None]:
-    user = await _get_user_by_email_for_auth(email=email, db=db)
-    if user is None:
-        print('not user')
-        return
-    if not Hasher.get_password_hash(password) == user.password:
-        print('not password')
-        return
-    return user
-
-
-@router.post('/token', response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(connect_db)):
-    user = await authenticate_user(form_data.username, form_data.password, db)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-        )
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.email, "other_custom_data": [1, 2, 3 ,4]}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-
-
-# @router.post('/login', name='user:login')
-# def login(user_form: UserLoginForm = Body(..., embed=True), database=Depends(connect_db)):
-#     user = database.query(User).filter(User.username == user_form.username).one_or_none()
-#     if not user or Hasher.get_password_hash(user_form.password) != user.password:
-#         return {'error': 'Email/password invalid'}
-#
-#     auth_token = AuthToken(token=str(uuid.uuid4()), user_id=user.id)
-#     database.add(auth_token)
-#     database.commit()
-#     return {'auth_token': auth_token.token}
-
-
-async def _create_new_user(body: UserCreate, db):
-    async with db as session:
-        async with session.begin():
-            exists_username_query = select(User.id).filter(User.username == body.username)
-            exists_username = await session.execute(exists_username_query)
-            if exists_username.one_or_none():
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Username already exists')
-            exists_email_query = select(User.id).filter(User.email == body.email)
-            exists_email = await session.execute(exists_email_query)
-            if exists_email.one_or_none():
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Email already exists')
-
-            user_dal = UserDAL(session)
-            user = await user_dal.create_user(
-                username=body.username,
-                password=body.password,
-                email=body.email
-            )
-            return {'user_id': user.id}
-
-
-@router.post('/user', name='user:create')
-async def create_user(body: UserCreate, db: AsyncSession = Depends(connect_db)):
-    return await _create_new_user(body, db)
+core_router = APIRouter()
 
 
 async def _get_film_by_id(film_id, db):
@@ -119,7 +31,7 @@ async def _get_film_by_id(film_id, db):
                 )
 
 
-@router.get('/films', response_model=ShowFilmSchema)
+@core_router.get('/films', response_model=ShowFilmSchema)
 async def get_film_by_id(film_id: int, db: AsyncSession = Depends(connect_db)):
     film = await _get_film_by_id(film_id, db)
     if film is None:
@@ -148,7 +60,7 @@ async def _get_series_by_id(series_id, db):
                 )
 
 
-@router.get('/series', response_model=ShowSeriesSchema)
+@core_router.get('/series', response_model=ShowSeriesSchema)
 async def get_series_by_id(series_id: int, db: AsyncSession = Depends(connect_db)):
     series = await _get_series_by_id(series_id, db)
     if series is None:
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, APIRouter
-from api.handlers import router
+from api.handlers import core_router
+from api.auth_handler import auth_router
 
 
 def get_application() -> FastAPI:
     application = FastAPI()
-    application.include_router(router)
+    application.include_router(core_router, prefix="/core", tags=["core"])
+    application.include_router(auth_router, prefix="/auth", tags=["auth"])
     return application
 
 
 app = get_application()
 
-router = APIRouter()
